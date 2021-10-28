e4415f7df80e96d25a6960f364c2ac4c52b2a3d5
kugue99A <kugue99A@users.noreply.github.com>
2021-10-28 15:01:12 +0000

    deploy: b06dfa52abd6e8f53601af842db599ee2fdb5645

---
+++ _next/static/Ws1w7raEBmy9q8Ig8-Gem/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8425f3b188d17b0455bf.css","static/chunks/pages/index-45256b615ae3eddd173c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
