d89505a5fb9a01d90969bd1097b313cc44f645df
Kugue99A <hisabisa99yohey@gmail.com>
2021-10-30 00:35:54 +0900

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import MainLayout from '../layouts';
-import styles from '../styles/layouts/index.module.scss';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
     <MainLayout>
-      <div>
+      <div className={ styles.container }>
         <h1>99A.</h1>
       </div>
     </MainLayout>
