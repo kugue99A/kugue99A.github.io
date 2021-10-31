cee94073c4cfb5db4e8be20e7adcdd9d340c1130
kugue99A <kugue99A@users.noreply.github.com>
2021-10-31 12:13:06 +0000

    deploy: 6cf311c34c89344d3feb8fd6dff256ce27536396

---
+++ _next/static/ss8pnZLvoJx24XNLbywg3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a544ce63530912af0c59.css","static/chunks/pages/index-14f4f1969d4acd56a8e3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/zOjYkXL99P1gSybf0_nWt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8273f482190a6b0c5f06.css","static/chunks/pages/index-b1c2b005d1fa74a0f43a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
