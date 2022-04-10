59e9f2185365819872d637119472b79134ad4289
kugue99A <kugue99A@users.noreply.github.com>
2022-04-10 11:04:55 +0000

    deploy: 838c474fbb2c6a0692b94c43dda765caacb5b6dc

---
+++ _next/static/pDU_D6QdUIN18ewGrDzt1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e4dc59918c8c2779.css","static/chunks/pages/index-02326d8d3b959ec2.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
