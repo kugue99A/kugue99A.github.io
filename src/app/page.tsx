400a8f9e00574e529011b2214c62304978581d3f
kugue99A <hisabisa99yohey@gmail.com>
2024-02-11 17:34:20 +0900

    [add] title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <Flex direction="column">
-          <h1 className="pa-4 underline">test test</h1>
-          <h1>test test</h1>
-          <h1>test test</h1>
-          <h1>test test</h1>
-          <h1>test test</h1>
+          <h1 className="">99A</h1>
         </Flex>
-        <h3 className="text-3xl font-bold underline">aAAAA</h3>
       </div>
     </main>
   )
